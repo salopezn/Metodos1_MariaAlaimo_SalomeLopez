60c3fe3b840610f5e172bc4b869edbb8c3c47784
mariaalaimo <m.alaimo@uniandes.edu.co>
2022-08-18 16:58:51 -0500

    Taller 1

---
+++ Taller1/Shell_7_modifactorial.sh
@@ -0,0 +1,14 @@
+fun_factorial (){
+        facto=1
+        for i in $(seq 1 1 $1)
+        do
+                facto=$(($facto*i))
+        done
+        echo $facto
+}
+read -p "Valor: " x
+for i in $(seq 1 1 $x)
+do
+	factor=$(fun_factorial $i)
+	echo $factor
+done
