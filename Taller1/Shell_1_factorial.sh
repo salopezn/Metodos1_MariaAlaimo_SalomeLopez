22d7dd7f36c3c780033af3871dd5d1e3c2816c53
mariaalaimo <m.alaimo@uniandes.edu.co>
2022-08-17 21:51:46 -0500

    Taller 1

---
+++ Taller1/Shell_1_factorial.sh
@@ -0,0 +1,14 @@
+read -p "Este programa calcula el factorial. Porfavor debe ingresar un numero para ejecutar: " x
+
+fun_factorial (){
+	facto=1
+	for i in $(seq 1 1 $x)
+	do
+		facto=$(($facto*i))
+	done
+	echo $facto
+
+
+
+}
+fun_factorial $x
