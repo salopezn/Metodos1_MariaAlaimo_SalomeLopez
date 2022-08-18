f2bc61f44662b9b87aeebfe436c8706d08e954d5
mariaalaimo <m.alaimo@uniandes.edu.co>
2022-08-18 17:03:12 -0500

    Taller 1

---
+++ Taller1/Shell_5_Variaciones.sh
@@ -0,0 +1,25 @@
+
+
+fun_factorial (){
+	
+	facto=1
+
+        for i in $(seq 1 1 $1)
+        do
+                facto=$(($facto*i))
+        done
+	echo $facto
+        
+
+
+
+}
+u=20
+n=$(fun_factorial $u)
+r=17
+m=$(fun_factorial $r)
+
+echo $((n/m))
+
+
+
